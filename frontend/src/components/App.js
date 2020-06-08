435ef863cace95d9b3d9291d11fd38ddf8fccd1b
Jeff Kramer <kramer.a.jeffrey@gmail.com>
2020-06-08 06:35:31 -0400

    Initial commit of App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,27 @@
 import React, {useEffect, useState} from 'react';
 import {render} from 'react-dom';
 
-const API_URL_BASE = 'poryphone.com/api/v1/';
+const API_URL_BASE = 'poryphone.com/api/v1';
 
 const App = () => {
     const [syncPairs, setSyncPairs] = useState({});
 
     useEffect(() => {
-        fetch(`http://`)
-        .then()
-    }, syncPairs)
+        fetch(`http://${API_URL_BASE}/syncpairs`)
+        .then(response => response.json)
+        .then(
+            (result) => {
+                setSyncPairs(result);
+            },
+            (error) => {
+                console.error(`Something went horribly wrong ${error}`);
+            }
+        )
+    }, [syncPairs]);
+
+    return ( 
+        <div>
+            
+        </div>
+    );
 }
