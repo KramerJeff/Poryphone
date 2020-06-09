1a879e44f2d8e01358f1e29143a732a2f80d5f2f
Jeff Kramer <kramer.a.jeffrey@gmail.com>
2020-06-09 16:13:52 -0400

    Remove DRF pagination

--- poryphonemanager/settings.py
+++ poryphonemanager/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -134,13 +133,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
-}
-
-# DATABASES['default'] = dj_database_url.config(
-#     conn_max_age=600, ssl_require=True)
 
 django_heroku.settings(locals())
