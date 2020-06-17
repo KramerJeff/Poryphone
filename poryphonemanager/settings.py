8267010649d5b2c0751d1adbcc0da6a75860dfea
Jeff Kramer <kramer.a.jeffrey@gmail.com>
2020-06-17 10:55:41 -0400

    Fix settings

--- poryphonemanager/settings.py
+++ poryphonemanager/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b9@4b$t*_va2g-@j+)jv^l!6x77psnlhjmxtf+-wl-!$ta62ff'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
